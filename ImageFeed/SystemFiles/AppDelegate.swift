6ed7f2d4b9fc9352a1186d00df6b5f84d01fead1
Roman Romanov <rromanov.vk@gmail.com>
2024-08-02 00:19:59 +0500

    ProgressHUD setting

--- ImageFeed/SystemFiles/AppDelegate.swift
+++ ImageFeed/SystemFiles/AppDelegate.swift
@@ -14,7 +14,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         ProgressHUD.animationType = .activityIndicator
         ProgressHUD.colorHUD = .black
-        ProgressHUD.colorAnimation = .darkGray
+        ProgressHUD.colorAnimation = .black
         return true
     }
 
